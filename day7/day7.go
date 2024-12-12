5ffe488051505756e5493216e9ccef8041072855
dveth <kanosauries@gmail.com>
2024-12-11 21:36:17 -0600

    finished day 7

--- day7/day7.go
+++ day7/day7.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"os"
 	"slices"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	part2()
+}
 
+func part2() {
+	file, err := os.Open("puzzleInput.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	var total uint64
+	count := 0
+	for scanner.Scan() {
+		equation, err := getEquationFromLine(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		if evaluateAll(equation.Nums, equation.TestValue) {
+			total += equation.TestValue
+		}
+		count++
+	}
+	fmt.Printf("Equation Count: %d\n", count)
+	fmt.Printf("Total: %d\n", total)
+}
+
+func testPart2() {
+	nums := []uint64{73, 91, 24, 78, 75, 93, 11, 36, 42, 86, 25, 24, 66, 26}
+	want := 13
+	got := getAllOperatorCombinations(len(nums))
+
+	if want != len(got) {
+		fmt.Printf("In evaluateAll for nums %v, wanted %v, got %v\n", nums, want, len(got[0]))
+	}
 }
 
 type Equation struct {
-	TestValue int
-	Nums      []int
+	TestValue uint64
+	Nums      []uint64
 }
 
 func (e Equation) equals(other Equation) bool {
@@ -32,17 +68,91 @@ func getEquationFromLine(line string) (Equation, error) {
 	}
 
 	numsSplit := strings.Split(strings.TrimSpace(lineSplit[1]), " ")
-	nums := []int{}
+	nums := []uint64{}
 
 	for _, str := range numsSplit {
 		value, err := strconv.Atoi(str)
 		if err != nil {
-			return Equation{}, errors.New("Non-integer value after : in line")
+			return Equation{}, errors.New("Non-uint64eger value after : in line")
 		}
-		nums = append(nums, value)
+		nums = append(nums, uint64(value))
 	}
 	return Equation{
-		TestValue: testValue,
+		TestValue: uint64(testValue),
 		Nums:      nums,
 	}, nil
+}
+
+// Evaluate if any combination of operators could work to make the values in nums equal the testValue
+// For example, if nums was {10, 19} then operators {"*"} would make the function return 'true'
+func evaluateAll(nums []uint64, testValue uint64) bool {
+	operatorCombinations := getAllOperatorCombinations(len(nums))
+	for _, operators := range operatorCombinations {
+		numsCopy := make([]uint64, len(nums))
+		copy(numsCopy, nums)
+		operatorsCopy := make([]string, len(operators))
+		copy(operatorsCopy, operators)
+		total, err := evaluate(numsCopy, operatorsCopy)
+		if err != nil {
+			continue
+		}
+		if total == testValue {
+			return true
+		}
+	}
+	return false
+}
+
+func evaluate(nums []uint64, operators []string) (uint64, error) {
+	if len(nums) != len(operators)+1 {
+		return 0, errors.New("Incorrect amount of operators for amount of nums.")
+	}
+	total := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if operators[i-1] == "+" {
+			total += nums[i]
+		}
+		if operators[i-1] == "*" {
+			total *= nums[i]
+		}
+		if operators[i-1] == "||" {
+			first := strconv.FormatUint(total, 10)
+			second := strconv.FormatUint(nums[i], 10)
+			var result uint64
+			fullString := first + second
+			result, err := strconv.ParseUint(fullString, 10, 0)
+			if err != nil {
+				return 0, err
+			}
+			total = result
+		}
+	}
+	return total, nil
+}
+
+func getAllOperatorCombinations(length int) [][]string {
+	if length == 0 || length == 1 {
+		return [][]string{}
+	}
+	if length == 2 {
+		return [][]string{
+			{"+"},
+			{"*"},
+			{"||"},
+		}
+	}
+	result := [][]string{}
+	innerResult := getAllOperatorCombinations(length - 1)
+	for _, value := range innerResult {
+		valueCopy := make([]string, len(value))
+		copy(valueCopy, value)
+		result = append(result, append(valueCopy, "+"))
+		valueCopy = make([]string, len(value))
+		copy(valueCopy, value)
+		result = append(result, append(valueCopy, "*"))
+		valueCopy = make([]string, len(value))
+		copy(valueCopy, value)
+		result = append(result, append(valueCopy, "||"))
+	}
+	return result
 }
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -1,12 +1,17 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"os"
+	"reflect"
+	"testing"
+)
 
 func TestGetEquationFromLine(t *testing.T) {
 	line := "190: 10 19"
 	want := Equation{
 		TestValue: 190,
-		Nums:      []int{10, 19},
+		Nums:      []uint64{10, 19},
 	}
 	got, err := getEquationFromLine(line)
 	if err != nil {
@@ -20,7 +25,7 @@ func TestGetEquationFromLine(t *testing.T) {
 	line = "161011: 16 10 13"
 	want = Equation{
 		TestValue: 161011,
-		Nums:      []int{16, 10, 13},
+		Nums:      []uint64{16, 10, 13},
 	}
 	got, err = getEquationFromLine(line)
 	if err != nil {
@@ -37,4 +42,186 @@ func TestGetEquationFromLine(t *testing.T) {
 	if err == nil {
 		t.Fatalf("Expected error in getEquationFromLine for input: %s\n", line)
 	}
+}
+
+func TestGetAllOperatorCombinations(t *testing.T) {
+	length := 2
+	want := [][]string{
+		{"+"},
+		{"*"},
+		{"||"},
+	}
+	got := getAllOperatorCombinations(length)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("In getAllOperatorCombinations for length %d, wanted %v, got %v\n", length, want, got)
+	}
+
+	length = 3
+	want = [][]string{
+		{"+", "+"},
+		{"+", "*"},
+		{"+", "||"},
+		{"*", "+"},
+		{"*", "*"},
+		{"*", "||"},
+		{"||", "+"},
+		{"||", "*"},
+		{"||", "||"},
+	}
+	got = getAllOperatorCombinations(length)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("In getAllOperatorCombinations for length %d, wanted %v, got %v\n", length, want, got)
+	}
+
+}
+
+func TestEvaluate(t *testing.T) {
+	nums := []uint64{10, 19}
+	operators := []string{"+"}
+	got, err := evaluate(nums, operators)
+	var want uint64
+	want = 29
+	if err != nil {
+		t.Fatalf("Received error in evaluate for nums %v and operators %v: %s\n", nums, operators, err.Error())
+	}
+
+	if want != got {
+		t.Fatalf("In evaluate for nums %v and operators %v, wanted %d, got %d\n", nums, operators, want, got)
+	}
+
+	nums = []uint64{10, 19}
+	operators = []string{"*"}
+	got, err = evaluate(nums, operators)
+	want = 190
+	if err != nil {
+		t.Fatalf("Received error in evaluate for nums %v and operators %v: %s\n", nums, operators, err.Error())
+	}
+
+	if want != got {
+		t.Fatalf("In evaluate for nums %v and operators %v, wanted %d, got %d\n", nums, operators, want, got)
+	}
+
+	nums = []uint64{81, 40, 27}
+	operators = []string{"+", "*"}
+	got, err = evaluate(nums, operators)
+	want = 3267
+	if err != nil {
+		t.Fatalf("Received error in evaluate for nums %v and operators %v: %s\n", nums, operators, err.Error())
+	}
+
+	if want != got {
+		t.Fatalf("In evaluate for nums %v and operators %v, wanted %d, got %d\n", nums, operators, want, got)
+	}
+
+	nums = []uint64{81, 40, 27}
+	operators = []string{"*", "+"}
+	got, err = evaluate(nums, operators)
+	want = 3267
+	if err != nil {
+		t.Fatalf("Received error in evaluate for nums %v and operators %v: %s\n", nums, operators, err.Error())
+	}
+
+	if want != got {
+		t.Fatalf("In evaluate for nums %v and operators %v, wanted %d, got %d\n", nums, operators, want, got)
+	}
+}
+
+func TestEvaluateAll(t *testing.T) {
+	testValue := uint64(190)
+	nums := []uint64{10, 19}
+	want := true
+	got := evaluateAll(nums, testValue)
+
+	if want != got {
+		t.Fatalf("In evaluateAll for nums %v and testValue %d, wanted %v, got %v\n", nums, testValue, want, got)
+	}
+
+	testValue = 3267
+	nums = []uint64{81, 40, 27}
+	want = true
+	got = evaluateAll(nums, testValue)
+
+	if want != got {
+		t.Fatalf("In evaluateAll for nums %v and testValue %d, wanted %v, got %v\n", nums, testValue, want, got)
+	}
+
+	testValue = 21037
+	nums = []uint64{9, 7, 18, 13}
+	want = false
+	got = evaluateAll(nums, testValue)
+
+	if want != got {
+		t.Fatalf("In evaluateAll for nums %v and testValue %d, wanted %v, got %v\n", nums, testValue, want, got)
+	}
+
+	testValue = 292
+	nums = []uint64{11, 6, 16, 20}
+	want = true
+	got = evaluateAll(nums, testValue)
+
+	if want != got {
+		t.Fatalf("In evaluateAll for nums %v and testValue %d, wanted %v, got %v\n", nums, testValue, want, got)
+	}
+
+	testValue = 11364228
+	nums = []uint64{46, 26, 2, 2, 9, 527}
+	want = true
+	got = evaluateAll(nums, testValue)
+
+	if want != got {
+		t.Fatalf("In evaluateAll for nums %v and testValue %d, wanted %v, got %v\n", nums, testValue, want, got)
+	}
+
+	testValue = 536008016
+	nums = []uint64{73, 91, 24, 78, 75, 93, 11, 36, 42, 86, 25, 24, 66, 26}
+	want = true
+	got = evaluateAll(nums, testValue)
+
+	if want != got {
+		t.Fatalf("In evaluateAll for nums %v and testValue %d, wanted %v, got %v\n", nums, testValue, want, got)
+	}
+
+	testValue = 156
+	nums = []uint64{15, 6}
+	want = true
+	got = evaluateAll(nums, testValue)
+
+	if want != got {
+		t.Fatalf("In evaluateAll for nums %v and testValue %d, wanted %v, got %v\n", nums, testValue, want, got)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	file, err := os.Open("testinput.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	var total uint64
+	count := 0
+	for scanner.Scan() {
+		equation, err := getEquationFromLine(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		if evaluateAll(equation.Nums, equation.TestValue) {
+			total += equation.TestValue
+		}
+		count++
+	}
+
+	wantCount := 9
+	if count != wantCount {
+		t.Fatalf("In part1, count should be %d, got %d\n", wantCount, count)
+	}
+
+	var wantTotal uint64
+	wantTotal = 11387
+	if wantTotal != total {
+		t.Fatalf("In part1, total should be %d, got %d\n", wantTotal, total)
+	}
+
 }
